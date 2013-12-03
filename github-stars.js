1a08ea79955c55a4b78139ed30627324f18657e7
jeremyfelt <jeremy.felt@gmail.com>
2013-12-02 22:28:59 -0800

    Move Javascript back into an anonymous function, add only what is necessary to global scope for jsonp callback

--- github-stars.js
+++ github-stars.js
@@ -1,28 +1,34 @@
 var access_token = '',
     gh_user      = '';
 
-function pageRequest( url ) {
-	$.ajax({
-		type: "GET",
-		url: url,
-		dataType: 'jsonp',
-		async: false,
-		jsonp: false
-	});
-}
+(function( window, $ ) {
+	var document = window.document;
 
-var processData = function( response ) {
-	var repo_date;
-	$.each( response.data, function processItem( key, val ) {
-		if ( 'WatchEvent' === val.type ) {
-			repo_date = new Date( val.created_at );
-			$('#stars-list').append( '<li>' + repo_date + ' ' + val.actor.login + ' <a href="' + val.repo.url + '">' + val.repo.name + '</a></li>' );
-		}
-	} );
-
-	if ( 'next' === response.meta.Link[0][1].rel ) {
-		pageRequest( response.meta.Link[0][0] );
+	function pageRequest( url ) {
+		$.ajax({
+			type: "GET",
+			url: url,
+			dataType: 'jsonp',
+			async: false,
+			jsonp: false
+		});
 	}
-};
 
-pageRequest( 'https://api.github.com/users/' + gh_user + '/received_events?access_token=' + access_token + '&callback=processData' );
+	var processData = function( response ) {
+		var repo_date;
+		$.each( response.data, function processItem( key, val ) {
+			if ( 'WatchEvent' === val.type ) {
+				repo_date = new Date( val.created_at );
+				$('#stars-list').append( '<li>' + repo_date + ' ' + val.actor.login + ' <a href="' + val.repo.url + '">' + val.repo.name + '</a></li>' );
+			}
+		} );
+
+		if ( 'next' === response.meta.Link[0][1].rel ) {
+			pageRequest( response.meta.Link[0][0] );
+		}
+	};
+
+	window.ghstars_processData = processData;
+
+	pageRequest( 'https://api.github.com/users/' + gh_user + '/received_events?access_token=' + access_token + '&callback=ghstars_processData' );
+})( window, jQuery );
