03ee64fb5f9ae41ec3d2c09a3748122e73500ed6
jeremyfelt <jeremy.felt@gmail.com>
2013-12-02 22:09:32 -0800

    Rewrite script to process as jsonp for auth to work, avoids rate limiting

--- github-stars.js
+++ github-stars.js
@@ -1,13 +1,27 @@
-(function( $ ) {
-	$.getJSON( 'https://api.github.com/users/jeremyfelt/received_events/public?page=1', processData );
-	function processData( data ) {
-		var star_list = '';
+var access_token = '',
+    gh_user      = '';
 
-		$.each( data, function processItem( key, val ) {
-			if ( 'WatchEvent' === val.type ) {
-				star_list += '<li>' + val.actor.login + ' <a href="' + val.repo.url + '">' + val.repo.name + '</a></li>';				
-			}
-		});
-		$('#stars-list').append( star_list );
+function pageRequest( url ) {
+	$.ajax({
+		type: "GET",
+		url: url,
+		dataType: 'jsonp',
+		async: false,
+		jsonp: false
+		//jsonpCallback: processData
+	});
+}
+
+var processData = function( response ) {
+	console.log( response );
+	$.each( response.data, function processItem( key, val ) {
+		if ( 'WatchEvent' === val.type ) {
+			$('#stars-list').append( '<li>' + val.actor.login + ' <a href="' + val.repo.url + '">' + val.repo.name + '</a></li>' );
+		}
+	} );
+	if ( 'next' === response.meta.Link[0][1].rel ) {
+		pageRequest( response.meta.Link[0][0] );
 	}
-})( jQuery );
+};
+
+pageRequest( 'https://api.github.com/users/' + gh_user + '/received_events?access_token=' + access_token + '&callback=processData' );
